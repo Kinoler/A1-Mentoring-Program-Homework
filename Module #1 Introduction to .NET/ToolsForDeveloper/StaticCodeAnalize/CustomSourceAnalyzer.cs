ed120db237fd69402fff988e2ec98273b01e18bc
raman_bortnik <raman_bortnik@epam.com>
2020-02-25 15:53:21 +0300

    Complate Static Code Analize task

---
+++ Module #1 Introduction to .NET/ToolsForDeveloper/StaticCodeAnalize/CustomSourceAnalyzer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StyleCop;
+using StyleCop.CSharp;
+
+namespace StaticCodeAnalize
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class CustomSourceAnalyzer : SourceAnalyzer
+    {
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            var csharpDocument = document as CsDocument;
+            if (csharpDocument != null)
+            {
+                csharpDocument.WalkDocument(
+                    new CodeWalkerElementVisitor<CustomSourceAnalyzer>(this.VisitElement),
+                    new CodeWalkerStatementVisitor<CustomSourceAnalyzer>(this.VisitStatement),
+                    new CodeWalkerExpressionVisitor<CustomSourceAnalyzer>(this.VisitExpression),
+                    this);
+            }
+        }
+
+
+        private bool VisitElement(CsElement element, CsElement parentElement, CustomSourceAnalyzer context)
+        {
+            if (element.ElementType != ElementType.Class)
+            {
+                return true;
+            }
+
+            if (((Class)element).BaseClass != "System.Web.Controller")
+            {
+                return true;
+            }
+
+            if (element.Name.EndsWith("Controller"))
+            {
+                return true;
+            }
+
+            context.AddViolation(element, "ClassInheritedByControllerShouldBeEndedByController", element.Declaration.Name);
+            return false;
+        }
+
+        private bool VisitStatement(Statement statement, Expression parentExpression, Statement parentStatement, CsElement parentElement, CustomSourceAnalyzer context)
+        {
+            // Add your code here.
+            return true;
+        }
+
+        private bool VisitExpression(Expression expression, Expression parentExpression, Statement parentStatement, CsElement parentElement, CustomSourceAnalyzer context)
+        {
+            // Add your code here.
+            return true;
+        }
+    }
+}
---
+++ Module #1 Introduction to .NET/ToolsForDeveloper/StaticCodeAnalize/Example.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticCodeAnalize
+{
+    class Example : System.Web.Controller
+    {
+
+    }
+}
+
+namespace System
+{
+    namespace Web
+    {
+        class Controller
+        {
+            
+        }
+    }
+}
