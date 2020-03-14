6b62141cf0412f9551f3e8303b580268494db87f
Raman Bortnik <raman_bortnik@epam.com>
2020-03-14 21:23:12 +0300

    Complate 2.3 module

---
+++ Module #2 C# Fundamentals/Exception Handling/IntParseLibrary/IntParse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntParseLibrary
+{
+    public class IntParse
+    {
+
+        public static int Parse(string str)
+        {
+            if (str == null)
+                throw new ArgumentNullException($"Argument {nameof(str)} can not be null");
+
+            if (ParseNumber(str, out var number))
+                throw new FormatException("Invalid string");
+
+            return number;
+        }
+
+        private static bool ParseNumber(string str, out int number)
+        {
+            bool negative = str[0] == '-';
+            int start = negative ? 1 : 0;
+            int num = 0;
+
+            for (int i = start; i < str.Length; i++)
+            {
+                char ch = str[i];
+
+                if (ch >= '0' && ch <= '9')
+                {
+                    checked
+                    {
+                        num *= 10;
+                        num += (ch - 48);
+                    }
+                }
+                else
+                {
+                    number = num;
+                    return true;
+                }
+            }
+
+            number = negative ? -num : num;
+            return false;
+        }
+    }
+}
---
+++ Module #2 C# Fundamentals/Exception Handling/ReadLine/ReadLineProgram.cs
@@ -0,0 +1,29 @@
+﻿using IntParseLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine
+{
+    internal class ReadLineProgram
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                var readLine = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(readLine))
+                {
+                    Console.WriteLine("You wrote an empty string.");
+                    continue;
+                }
+
+                var firstSymbol = readLine[0];
+                Console.WriteLine($"{firstSymbol}");
+            }
+        }
+    }
+}
