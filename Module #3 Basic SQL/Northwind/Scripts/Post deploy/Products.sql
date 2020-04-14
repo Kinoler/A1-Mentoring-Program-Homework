d1722173489b625845623e50806ad6ef411da7d7
Raman Bortnik <raman_bortnik@epam.com>
2020-04-14 20:49:48 +0300

    SQL Added deleting all data before inserting

--- Module #3 Basic SQL/Northwind/Scripts/Post deploy/Products.sql
+++ Module #3 Basic SQL/Northwind/Scripts/Post deploy/Products.sql
@@ -1,4 +1,5 @@
-﻿SET IDENTITY_INSERT [Northwind].[Products] ON
+﻿TRUNCATE TABLE [Northwind].[Products]
+SET IDENTITY_INSERT [Northwind].[Products] ON
 INSERT [Northwind].[Products] ([ProductID], [ProductName], [SupplierID], [CategoryID], [QuantityPerUnit], [UnitPrice], [UnitsInStock], [UnitsOnOrder], [ReorderLevel], [Discontinued]) VALUES (1, N'Chai', 1, 1, N'10 boxes x 20 bags', 18.0000, 39, 0, 10, 0)
 INSERT [Northwind].[Products] ([ProductID], [ProductName], [SupplierID], [CategoryID], [QuantityPerUnit], [UnitPrice], [UnitsInStock], [UnitsOnOrder], [ReorderLevel], [Discontinued]) VALUES (2, N'Chang', 1, 1, N'24 - 12 oz bottles', 19.0000, 17, 40, 25, 0)
 INSERT [Northwind].[Products] ([ProductID], [ProductName], [SupplierID], [CategoryID], [QuantityPerUnit], [UnitPrice], [UnitsInStock], [UnitsOnOrder], [ReorderLevel], [Discontinued]) VALUES (3, N'Aniseed Syrup', 1, 2, N'12 - 550 ml bottles', 10.0000, 13, 70, 25, 0)
