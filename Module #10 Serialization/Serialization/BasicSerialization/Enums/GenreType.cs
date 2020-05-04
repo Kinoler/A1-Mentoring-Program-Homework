466a647c9aa3a67ce97ab01c8a9ee8e86b8325dc
Raman Bortnik <raman_bortnik@epam.com>
2020-05-04 16:08:38 +0300

    Serialization Completed basic serialization module.

---
+++ Module #10 Serialization/Serialization/BasicSerialization/Enums/GenreType.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace BasicSerialization.Enums
+{
+    public enum GenreType
+    {
+        [XmlEnum]
+        Computer,
+        [XmlEnum]
+        Fantasy,
+        [XmlEnum]
+        Romance,
+        [XmlEnum]
+        Horror,
+        [XmlEnum("Science Fiction")]
+        ScienceFiction
+
+    }
+}
---
+++ Module #10 Serialization/Serialization/BasicSerialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using BasicSerialization.Models;
+
+namespace BasicSerialization
+{
+    public class Program
+    {
+        public const string XmlFrom = "books.xml";
+        public const string XmlTo = "booksSerialized.xml";
+
+        public static void Main()
+        {
+            var serializer = new XmlSerializer(typeof(Catalog), "http://library.by/catalog");
+            Catalog deserializedCatalog = null;
+
+            using (var fileStream = File.OpenRead(XmlFrom))
+            {
+                deserializedCatalog = (Catalog)serializer.Deserialize(fileStream);
+            }
+
+            using (var fileStream = File.OpenWrite(XmlTo))
+            {
+                serializer.Serialize(fileStream, deserializedCatalog);
+            }
+        }
+    }
+}
