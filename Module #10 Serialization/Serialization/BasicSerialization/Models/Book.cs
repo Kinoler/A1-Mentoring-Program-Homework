7bcdca349e299991da5c7b0053203fb073e260a2
Raman Bortnik <raman_bortnik@epam.com>
2020-05-12 18:20:14 +0300

    Serialization Fixed DataType in Book model

--- Module #10 Serialization/Serialization/BasicSerialization/Models/Book.cs
+++ Module #10 Serialization/Serialization/BasicSerialization/Models/Book.cs
@@ -30,26 +30,13 @@ namespace BasicSerialization.Models
         [XmlElement("publisher")]
         public string Publisher { get; set; }
 
-        [XmlElement("publish_date")]
-        public string PublishDateString {
-            get => this.PublishDate.ToString("yyyy-MM-dd");
-            set => this.PublishDate = DateTime.Parse(value);
-        }
+        [XmlElement("publish_date", DataType = "date")]
+        public DateTime PublishDate { get; set; }
 
         [XmlElement("description")]
         public string Description { get; set; }
 
-        [XmlElement("registration_date")]
-        public string RegistrationDateString
-        {
-            get => this.RegistrationDate.ToString("yyyy-MM-dd");
-            set => this.RegistrationDate = DateTime.Parse(value);
-        }
-
-        [XmlIgnore]
-        public DateTime PublishDate { get; set; }
-
-        [XmlIgnore]
+        [XmlElement("registration_date", DataType = "date")]
         public DateTime RegistrationDate { get; set; }
     }
 }
